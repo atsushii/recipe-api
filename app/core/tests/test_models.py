d804bc625cc3e08cd198ff241bd7a0b227d1c295
Atsushi <atsushimiyamoto07@gmail.com>
2021-01-20 14:55:01 -0800

    Test create admin

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -29,7 +30,7 @@ class ModelTests(TestCase):
 
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None, 'test1234')
-    
+
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
 
